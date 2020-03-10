d56bf87900dd7d42c9feeeeecbbe8dd2fbf4c5d6
Joaquin Torre <joa.dev@live.com>
2020-03-10 14:59:38 -0300

    Add contacts graph option in generate_dataset
    Saves graphs to graphs/contacts
    
    TODO: parallelize, it's way too slow atm
    TODO: save get_contacts IFs

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -4,12 +4,13 @@ import os
 import sys
 import argparse
 import requests
+import tempfile
 import warnings
 import numpy as np
 import networkx as nx
 import pickle as pkl
 import progressbar
-
+import logging
 import biograph.graph_models as graph_models
 from biograph.downloader import PdbDownloader, ConsurfDBDownloader
 from biograph.protein import Protein
@@ -26,19 +27,24 @@ parser.add_argument('--laptop-safe', dest='laptop_safe', action='store_true',
                     help='do not process proteins with > 200 ATP connections')
 parser.add_argument('--include-interior-points', dest='include_interior', action='store_true',
                     help='do not process proteins with > 200 ATP connections')
+parser.add_argument('--contacts_graph', dest='contacts_graph', action='store_true',
+                    help='use a contacts graph instead of a surface graph')
 
 parser.add_argument('--skip_graph_generation', dest='skip_graph_generation', action="store_true",
                     help="skip graph generation altogether")
 
+logging.basicConfig(level=logging.DEBUG, filename="generate_dataset.log")
+#logging.getLogger().addHandler(logging.StreamHandler())
 
 args = parser.parse_args()
+logging.info("Called with args {}".format(args))
 
 # Download datasets.
 if not os.path.exists("data/consurf"):
     os.makedirs("data/consurf")
 
-if not os.path.exists("graphs/"):
-    os.makedirs("graphs/")
+if not os.path.exists("graphs/contacts"):
+    os.makedirs("graphs/contacts")
 
 if not args.skip_pdb_download:
     with open("pdb_atp_list.txt", "r") as f:
@@ -54,6 +60,8 @@ else:
 if any(map(lambda fn: fn is None, filenames)):
     print("Some files could not be downloaded")
 
+if args.contacts_graph:
+    tempdir = tempfile.TemporaryDirectory()
 #filenames = ["data/3KEU.pdb", "data/3ULE.pdb", "dlata/4DQW.pdb"]
 # Searching a bit I didn't find a way to get a chain list or get all
 # conservation features for a specific protein, so we need to look up
@@ -127,36 +135,52 @@ for i, fn in progressbar.progressbar(enumerate(filenames), max_value=len(filenam
     # Only keep chains that are connected to the ligand.
     chains_with_ligand = protein.df[protein.df.distance <= 6.0].chain.unique()
     protein.select_chains(chains_with_ligand)
-
-    # Generate the graph.
-    structure = protein.generate_structure(lambda row: row["full_id"][4][0] == "CA")
-
-    perseus = Perseus()
-    perseus.execute_persistent_hom(protein)
-
-    structure_model = graph_models.StructureGraphGenerator()
-    protein.generate_graph(structure_model,
-        {"step": structure.persistent_hom_params["b3_step"],
-         "surface_only": not args.include_interior})
-
-    # Depth features
-    depths, _ = structure.calculate_depth(protein.graph)
-    for node_idx, depth in depths.items():
-        protein.graph.nodes[node_idx]["depth"] = depth
-
     # Identify chain uniquely (useful later to match with CDHit)
     protein.df["chain"] = protein.pdb.id + "_" + protein.df.chain
-    # Rest of features
-    structure_model.add_features(protein.df, columns = [
-        "bfactor", "score", "color",
-        "color_confidence_interval_high", "color_confidence_interval_low",
-        "score_confidence_interval_high", "score_confidence_interval_low",
-        "resname", "coord", "distance",
-        "chain"
-    ])
+    if args.contacts_graph:
+        scgg = graph_models.StaticContactGraphGenerator()
+        try:
+            protein.generate_graph(scgg, {
+                "save_contact_filename": f"{tempdir.name}/{protein.pdb.id}_contacts.tsv"
+            })
+        except Exception as e:
+            logging.info("Error on protein #{} ({}): {}".format(i, fn, e))
+            continue
+        scgg.add_features(protein.df, columns = [
+            "x", "y", "z", "bfactor", "chain", "occupancy", "distance"
+        ])
+        graph = protein.graph
+        dest_pickle = "graphs/contacts/{}.pkl".format(protein.pdb.id)
+    else:
+        # Generate the graph.
+        structure = protein.generate_structure(lambda row: row["full_id"][4][0] == "CA")
 
-    dest_pickle = "graphs/{}.pkl".format(protein.pdb.id)
-    nx.write_gpickle(structure_model.G, dest_pickle)
+        perseus = Perseus()
+        perseus.execute_persistent_hom(protein)
+
+        structure_model = graph_models.StructureGraphGenerator()
+        protein.generate_graph(structure_model,
+            {"step": structure.persistent_hom_params["b3_step"],
+            "surface_only": not args.include_interior})
+
+        # Depth features
+        depths, _ = structure.calculate_depth(protein.graph)
+        for node_idx, depth in depths.items():
+            protein.graph.nodes[node_idx]["depth"] = depth
+
+        # Rest of features
+        structure_model.add_features(protein.df, columns = [
+            "bfactor", "score", "color",
+            "color_confidence_interval_high", "color_confidence_interval_low",
+            "score_confidence_interval_high", "score_confidence_interval_low",
+            "resname", "coord", "distance",
+            "chain"
+        ])
+
+        dest_pickle = "graphs/{}.pkl".format(protein.pdb.id)
+        graph = structure_model.G
+
+    nx.write_gpickle(graph, dest_pickle)
     #print("Exported graph to {}".format(dest_pickle))
 
 
